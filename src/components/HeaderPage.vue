0f8acc4f5e5397da8b2e02573d11d080794a8b62
yaroslavyerm <yaroslav.yermakov@edu.univ-fcomte.fr>
2024-05-15 12:13:26 +0200

    header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-
+import { RouterLink } from 'vue-router/auto'
+import LogoIcon from './icons/LogoIcon.vue';
 </script>
 
 <template>
-  <p class="text-blue-500 font-fixel font-bold text-lg italic">header</p>
+  <div class="bg-indigo-900 w-full pl-4 pt-3 pb-1 top-0 left-0 drop-shadow sticky">
+    <RouterLink to="/" class="w-[245px]">
+        <LogoIcon class="max-w-full"/>
+    </RouterLink>
+  </div>
 </template>
