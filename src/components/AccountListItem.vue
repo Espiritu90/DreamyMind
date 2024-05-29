390100eeb0746657a372c5d62489713efffa4430
yaroslavyerm <yaroslav.yermakov@edu.univ-fcomte.fr>
2024-05-29 11:31:37 +0200

    idk

--- src/components/AccountListItem.vue
+++ src/components/AccountListItem.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+    userAvatar?: string
+    username?: string
+}>()
+</script>
+
 <template>
-    <img src="../../public/img/avatar.png" alt="avatar" class="rounded-full w-8 h-8"/> 
-    <p>{{ username }}</p>
-    
+    <div class="flex gap-2 my-1">
+        <img src="../../public/img/avatar.png" alt="avatar" class="rounded-full w-8 h-8"/>
+        <p class="my-auto">{{ username }}</p>
+        <!--change to RouterLink-->
+    </div>
 </template>
---
+++ src/pages/en/following.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AccountListItem from '../../components/AccountListItem.vue'
+</script>
+
+<template>
+    <div class="wrapper">
+        <h1>Following</h1>
+        <div class="my-3 bg-indigo-900 rounded-[32px] pt-8 p-5 flex flex-col">
+            <AccountListItem v-for="n in 10" :key="n" username="sleepyuser145"/>
+        </div>
+        <button class="">Unfollow</button>
+    </div>
+</template>
