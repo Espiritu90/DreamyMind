122025b6c6acabefff5454c00503c8570a269ec5
yaroslavyerm <yaroslav.yermakov@edu.univ-fcomte.fr>
2024-06-06 13:52:07 +0200

    Merge branch 'main' of https://github.com/Espiritu90/DreamyMind

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -43,46 +43,48 @@ const variantClass ={
 <template>
     
     <div class="my-3 bg-indigo-900 rounded-[32px] pt-8 p-5"
-   >
+    :class=variantClass[props.variant]>
     <!--the dream of the week with moon icon-->
-        <div class="flex justify-between" >
+        <div class="flex justify-between" :class="props.variant === 'home'? 'visible' : 'hidden'">
             <h3>The dream of the week</h3>
             <MoonIcon class="h-max-6"/>
         </div>
 
         <!--separator-->
-        <div class="block h-0.5 w-full rounded-full my-2 bg-amber-100" ></div>
+        <div class="block h-0.5 w-full rounded-full my-2 bg-amber-100" :class="props.variant === 'home'? 'visible' : 'hidden' "></div>
         <div>
 
 <!--avatar + username-->
                 <div class="flex align-middle gap-2"
-                >
-                    <img src="../../public/img/avatar.png" alt="avatar" class="h-8 w-max-full"> <!--change to if avatar=n src=****-->
+                :class="props.variant === 'home' || props.variant === 'feed' ? 'visible' : 'hidden'">
+                    <img :src="`${userAvatar}`" alt="avatar" class="h-8 w-max-full">
                     <p class="my-auto">{{ user }}</p>
             </div>
 
 <!--separator-->
-             <div class="block h-0.5 w-full rounded-full my-2 bg-amber-100" :class="variant === 'feed'? 'visible' : 'hidden'"></div>
+             <div class="block h-0.5 w-full rounded-full my-2 bg-amber-100" :class="props.variant === 'feed'? 'visible' : 'hidden'"></div>
 
              <!--published-->
 
-             <div :class="props.variant==='journal_public'? 'visible' : 'hidden'" class=" w-fit  flex align-middle justify-center">
+             <div :class="published? 'visible' : 'hidden'" class=" w-fit  flex align-middle justify-center">
                 <p class="text-indigo-900 m-auto text-sm align-middle bg-amber-100 rounded-full px-1 py-1.5 leading-3 font-normal">Published</p>
              </div>
            
              <!--title + text-->
              <div class="px-3 pt-2">
                 <h2>{{ title }}</h2>
-                <p class="bg-gradient-to-t from-transparent from-[1%] via-amber-100 via-30% to-amber-100 text-transparent bg-clip-text line-clamp-6">{{textDream}}</p>
+                <p class="bg-gradient-to-t from-transparent from-[1%] via-amber-100 via-30% to-amber-100 text-transparent bg-clip-text line-clamp-6">{{text}}</p>
             </div>
 
             <!--continue reading-->
             <RouterLink to="/posts/[id]" class="text-base text-amber-100 bg-indigo-900 h-fit"><p class="font-semibold text-center">Continue reading</p></RouterLink>
 
 <!--like+comment-->
-            <div :class="props.variant === 'journal_private' || props.variant==='home' ? 'hidden' : 'visible'" class="flex gap-5">
-                <div class="flex gap-1">
-                    <LikeIcon/>
+            <div :class="published || props.variant==='home' ? 'hidden' : 'visible'" class="flex gap-5">
+                <div class="flex gap-1 items-center">
+                    <LikeIcon @click="toggleLike"
+        :class="{'fill-amber-100': isLiked}" 
+        class="w-6 h-auto cursor-pointer stroke-amber-100" />       
                     <p>{{ likes }}</p>
                 </div>
                 <div class="flex gap-1">
